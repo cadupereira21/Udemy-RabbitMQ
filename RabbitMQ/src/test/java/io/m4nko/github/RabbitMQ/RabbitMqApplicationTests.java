418f52e5ff5878b10b6cf06b2f81f54c7e5ecce8
Carlos Pereira <carloseduardo2101@gmail.com>
2023-05-16 10:45:37 -0300

    Create: generated spring project and configured rabbit properties

---
+++ RabbitMQ/src/test/java/io/m4nko/github/RabbitMQ/RabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package io.m4nko.github.RabbitMQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
