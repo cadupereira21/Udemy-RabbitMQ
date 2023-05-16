9208f941443a69236dd6137ca93215b1c0621b00
Carlos Pereira <carloseduardo2101@gmail.com>
2023-05-16 14:02:03 -0300

    Create: implemented new method to consume headers-exchange messages

--- RabbitMQ/src/main/java/io/m4nko/github/RabbitMQ/service/RabbitConsumer.java
+++ RabbitMQ/src/main/java/io/m4nko/github/RabbitMQ/service/RabbitConsumer.java
@@ -4,6 +4,11 @@ import io.m4nko.github.RabbitMQ.model.Person;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+
 @Service
 public class RabbitConsumer {
 
@@ -11,4 +16,16 @@ public class RabbitConsumer {
     public void getMessage(Person p){
         System.out.println("Person name on mobile queue: " + p.getName());
     }*/
+
+    @RabbitListener(queues = "Mobile")
+    public void getMessage(byte[] message) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream bis = new ByteArrayInputStream(message);
+        ObjectInput in = new ObjectInputStream(bis);
+
+        Person p = (Person) in.readObject();
+        in.close();
+        bis.close();
+
+        System.out.println("Person name on mobile queue: " + p.getName());
+    }
 }
